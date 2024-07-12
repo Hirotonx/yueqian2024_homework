7ad072d58b71a6766c6d14df1a885f399b256ec0
Hiroton <1714451114@qq.com>
2024-07-12 20:20:00 +0800

    fix name mistake

---
+++ 2024.7.12/5数排序.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+std::vector<int> quick_sort(const std::vector<int>& arr)//&引用,防止重复赋值
+{
+    int len = arr.size();
+    if(len == 1){return arr;}
+    int mid = arr[0];
+
+    std::vector<int> left;
+    std::vector<int> right;
+    for(int i = 1;i < arr.size();i++){
+        if (arr[i] <= mid){left.push_back(arr[i]);}
+        else{{right.push_back(arr[i]);}}
+    }
+
+    left = quick_sort(left);
+    right = quick_sort(right);
+
+    std::vector<int> res;
+    res.reserve(left.size() + right.size() + 1);
+    res.insert(res.end(), left.begin(), left.end());
+    res.push_back(mid);
+    res.insert(res.end(), right.begin() ,right.end());
+
+    return res;
+}
+int main()
+{
+    std::vector<int> input_arr;
+    int tmp;
+
+    std::cout<<"输入5个整数"<<std::endl;
+    for(int i = 0; i < 5; i++){
+        std::cin>>tmp;
+        input_arr.push_back(tmp);
+    }
+
+    std::vector<int> res_arr = quick_sort(input_arr);
+
+    std::cout<<"最大值: "<< res_arr[4]<<"最小值: "<<res_arr[0]<<std::endl;
+
+}
--- 2024.7.12/打印砖石.c
+++ 2024.7.12/打印砖石.c
@@ -9,19 +9,22 @@ int main()
     int mid_line = input_number;
     input_number = input_number >> 1;
     int i = 1;
-    
+    int extar = 0;
+    if(mid_line % 2 == 1){extar++;}
+
     for(i;i<=input_number;i++){
-        space_number = input_number + 1 - i;//+1为了处理奇数行设计的冗余
+        space_number = input_number + extar - i;//+1为了处理奇数行设计的冗余
         while(space_number){printf(" ");space_number--;}
         //if(index%2==0){index+=1;}
         star_number = i;
-        star_number=star_number*2-1;
+        star_number = star_number*2 - 1;
         while(star_number){printf("*");star_number--;}
         printf("\n");
     }
-    i = i-1;
-    if (mid_line % 2!=0){
-        space_number =input_number - (mid_line>>1);
+
+    i--;
+    if (mid_line % 2 != 0){
+        space_number = input_number - (mid_line >> 1);
         star_number = mid_line;
         while(space_number){printf(" ");space_number--;}
         while(star_number){printf("*");star_number--;}
@@ -29,11 +32,11 @@ int main()
     }
 
         for(i;i>=1;i--){
-        space_number = input_number + 1 - i;
+        space_number = input_number + extar - i;
         while(space_number){printf(" ");space_number--;}
         //if(index%2==0){index+=1;}
         star_number = i;
-        star_number=star_number*2-1;
+        star_number = star_number*2 - 1;
         while(star_number){printf("*");star_number--;}
         printf("\n");
     }
